09a12116e4bbf2a630f2ce305c55882a1d56f08c
이서연 <iseoyeon@pyowonsig-ui-MacBookAir.local>
2025-01-19 11:42:24 +0900

    Feat: await 추가

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   try {
     console.log('Cron job triggered');
-    groupPurchaseStatusCheck();
+    await groupPurchaseStatusCheck(); // 비동기 함수일 경우 await 사용
     return NextResponse.json({ message: 'Cron job scheduled successfully' });
   } catch (error) {
     console.error('Error executing cron job:', error);
